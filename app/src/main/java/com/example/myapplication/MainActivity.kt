9a4c8c03c77b31f683419964369db52989b359f4
sohir <sohirhashem@gmail.com>
2020-04-12 10:07:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun testUpload(){
+        val a :Int = 3
+        val b:Int = 6
+        val result: Int = a +b
+    }
+}
